6cf1dc6ec0c90dfb1cc9783079d21c2ca04697f3
kasper <kasper@minox.nl>
2019-04-09 21:02:02 +0200

    generalize naming

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_NAME,
+  process.env.POSTGRES_DB,
   process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
