813759b2866a88841e49497c6ce830050bb65591
kasper <kasper@minox.nl>
2019-04-09 21:06:16 +0200

    get port from env instead of hardcoding it

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const db = require("./db/db");
 
 db.init();
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,5 +11,5 @@ app.get("/", (req, res) => {
   res.send("it's working :)");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port: ${port}.`));
